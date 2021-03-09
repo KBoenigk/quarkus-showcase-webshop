822df1436e6e375eb4de299c529196cce637b64a
DESKTOP-L1I5TE1\kim.boenigk <kim.boenigk@openknowledge.de>
2021-03-09 16:17:56 +0100

    added some Produkte to Database

--- src/main/resources/db/migration/webshop/V1.0.0__Webshop.sql
+++ src/main/resources/db/migration/webshop/V1.0.0__Webshop.sql
@@ -6,7 +6,8 @@ CREATE TABLE public.PRODUKT (
                                  BESCHREIBUNG VARCHAR(64)
 );
 
-INSERT INTO public.PRODUKT (ID, NAME, PREIS, BESCHREIBUNG) VALUES (1, 'Marco', 0, 'Polo');
-INSERT INTO public.PRODUKT (ID, NAME, PREIS, BESCHREIBUNG) VALUES (2, 'Ping', 0,'Pong');
-INSERT INTO public.PRODUKT (ID, NAME, PREIS, BESCHREIBUNG) VALUES (3, 'Moin', 0,'Moin');
-INSERT INTO public.PRODUKT (ID, NAME, PREIS, BESCHREIBUNG) VALUES (4, 'Hi', 0,'Hey');
+INSERT INTO public.PRODUKT (ID, NAME, PREIS, BESCHREIBUNG) VALUES (1, 'Falafel Schmaus', 9.8, 'Ein leckerer Salat.');
+INSERT INTO public.PRODUKT (ID, NAME, PREIS, BESCHREIBUNG) VALUES (2, 'Rosinenbomber Bowl', 11.4, 'Eine bunte Bowl.');
+INSERT INTO public.PRODUKT (ID, NAME, PREIS, BESCHREIBUNG) VALUES (3, 'Ciabatta Sticks', 1.0, 'Eine sättigende Beilage.');
+INSERT INTO public.PRODUKT (ID, NAME, PREIS, BESCHREIBUNG) VALUES (4, 'Bananenbrot', 2.4,'Eine süße Nachspeise.');
+INSERT INTO public.PRODUKT (ID, NAME, PREIS, BESCHREIBUNG) VALUES (5, 'Lemonaid Maracuja', 2.5,'Eine erfrischende Limo.');
