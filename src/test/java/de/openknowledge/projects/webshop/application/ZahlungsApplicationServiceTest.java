09bb2b14f9440b637b7ad0c2a183b830c78e02f6
kim.boenigk <kim.boenigk@openknowledge.de>
2021-03-16 13:52:05 +0100

    added test for ZahlungsApplicationService

--- src/test/java/de/openknowledge/projects/webshop/application/ZahlungsApplicationServiceTest.java
+++ src/test/java/de/openknowledge/projects/webshop/application/ZahlungsApplicationServiceTest.java
@@ -8,28 +8,35 @@ import de.openknowledge.projects.webshop.domain.bestellung.ProduktListenElement;
 import de.openknowledge.projects.webshop.domain.bestellung.lieferung.LieferAdresse;
 import de.openknowledge.projects.webshop.domain.zahlung.Zahlung;
 import de.openknowledge.projects.webshop.infrastructure.zahlung.ZahlungsRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class ZahlungsApplicationServiceTest {
 
-    @InjectMocks
     private ZahlungsApplicationService zahlungsService;
 
-    @Mock
     private ZahlungsRepository zahlungsRepository;
 
     private Zahlung zahlung;
 
+    @Captor
+    private ArgumentCaptor<Zahlung> captor;
+
     @BeforeEach
     public void setUp() {
+
+        this.zahlungsRepository = Mockito.mock(ZahlungsRepository.class);
+        this.zahlungsService = new ZahlungsApplicationService(this.zahlungsRepository);
 
         Produkt bowl = Produkt.Builder().setProduktName("Bowl").setProduktPreis(BigDecimal.valueOf(8.4)).build();
 
@@ -53,14 +60,18 @@ public class ZahlungsApplicationServiceTest {
 
         this.zahlung = Zahlung.Builder().setBestellung(bestellung)
                                         .build();
+
+        this.zahlungsRepository.putZahlung(this.zahlung);
     }
 
     @Test
-    public void autorisiereZahlungShouldAuthorizeZahlungInRepository() {
-//        Mockito.when(this.zahlungsRepository.findZahlungByID(this.zahlung.getZahlungsId())).thenReturn(Optional.of(this.zahlung));
-//
-//        this.zahlungsService.autorisiereZahlung(this.autorisierung);
-//
-//        Assertions.assertThat(this.zahlungsRepository.read().get(0).getAutorisierung().isAutorisiert()).isTrue();
+    public void autorisiereZahlungShouldUpdateAuthorizedZahlung() {
+        Mockito.when(this.zahlungsRepository.findZahlungByID(this.zahlung.getZahlungsId())).thenReturn(Optional.of(this.zahlung));
+
+        this.zahlungsService.autorisiereZahlung(this.zahlung.getZahlungsId().getId());
+
+        Mockito.verify(this.zahlungsRepository).updateZahlung(captor.capture());
+        Zahlung capturedArgument = captor.getValue();
+        Assertions.assertThat(capturedArgument.getAutorisierung().isAutorisiert()).isTrue();
     }
 }
